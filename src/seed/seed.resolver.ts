c6d369cffca96a2ab54fcae8e7c919d028fc7eb6
Sherwin Gonzalez <paintsis@gmail.com>
2023-04-14 17:02:47 -0600

    rest

--- src/seed/seed.resolver.ts
+++ src/seed/seed.resolver.ts
@@ -1,7 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver } from '@nestjs/graphql';
 import { SeedService } from './seed.service';
 
 @Resolver()
 export class SeedResolver {
   constructor(private readonly seedService: SeedService) {}
+  @Mutation()
+  async executeSeed(): Promise<boolean>{
+    return true
+  }
+
 }
